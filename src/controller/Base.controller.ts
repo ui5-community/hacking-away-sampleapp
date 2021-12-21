1710b9683c84adcb5b99c1735b06a687715581a2
Peter Muessig <peter.muessig@sap.com>
2021-12-21 18:19:23 +0100

    Use Web Components

--- src/controller/Base.controller.ts
+++ src/controller/Base.controller.ts
@@ -1,7 +1,15 @@
 import Controller from "sap/ui/core/mvc/Controller"
+import JSONModel from "sap/ui/model/json/JSONModel";
+import { UIModelData } from "../types/ui"
 
 export default class BaseController extends Controller {
 
+	protected uiModelData: UIModelData;
+
 	public onInit() : void {
-		}
+
+		this.uiModelData = (this.getOwnerComponent().getModel("ui") as JSONModel).getData() as UIModelData;
+
+	}
+
 }
--- src/controller/Main.controller.ts
+++ src/controller/Main.controller.ts
@@ -1,9 +1,26 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access,
+                  @typescript-eslint/no-unsafe-call */
+
+import Event from "sap/ui/base/Event";
 import BaseController from "./Base.controller";
+
+import axios, {AxiosResponse } from "axios";
+import JSONModel from "sap/ui/model/json/JSONModel";
 
 export default class MainController extends BaseController {
 
 	public onInit() : void {
-        super.onInit()
+		super.onInit()
+	}
+
+	public async addTodo(event: Event): Promise<void> {
+		await axios.post('/json-server/todos', {
+			id: Math.round(Math.random() * 100000),
+			title: this.uiModelData.new.title
+		});
+		const response = await axios.get('/json-server/todos') as AxiosResponse;
+		(this.getOwnerComponent().getModel() as JSONModel).setData(response.data);
+		this.uiModelData.new.title = "";
 	}
 
 }
